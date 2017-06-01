aaac8b09bc011b061a83ee756354bf8f293df43e
AgentFire <agentfire@mail.ru>
2017-06-01 16:41:47 +0300

    first!

---
+++ AgentFire.Lifetime.ConsoleApp/CApp.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace AgentFire.Lifetime.ConsoleApp
+{
+    public static class CApp
+    {
+        public static void Lifetime(string name, Action start, Action stop, bool stopOnFailedStart = true)
+        {
+            Console.WriteLine($"Launching {name} in console mode...");
+            Console.WriteLine("--");
+
+            Console.TreatControlCAsInput = true;
+
+            GoInternal(name, start, stop, stopOnFailedStart);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey(true);
+        }
+
+        private static void GoInternal(string name, Action start, Action stop, bool stopOnFailedStart)
+        {
+            try
+            {
+                start();
+            }
+            catch
+            {
+                bool failedToStopToo = false;
+
+                if (stopOnFailedStart)
+                {
+                    try
+                    {
+                        stop();
+                    }
+                    catch
+                    {
+                        failedToStopToo = true;
+                    }
+                }
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"{name} has failed to start");
+
+                if (failedToStopToo)
+                {
+                    Console.Write($" and to stop after that");
+                }
+
+                Console.WriteLine(".");
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+                return;
+            }
+
+            Console.WriteLine("--");
+            Console.WriteLine("Startup is successful. Press Ctrl-C to shutdown.");
+            Console.WriteLine("--");
+
+            ConsoleKeyInfo info;
+            do
+            {
+                info = Console.ReadKey(true);
+            } while (info.Key != ConsoleKey.C || info.Modifiers != ConsoleModifiers.Control);
+
+            Console.WriteLine("--");
+
+            try
+            {
+                stop();
+            }
+            catch
+            {
+                Console.WriteLine($"{name} has failed to stop.");
+            }
+
+            Console.WriteLine("--");
+            Console.WriteLine($"{name} is no more.");
+        }
+    }
+}
---
+++ Test App/Program.cs
@@ -0,0 +1,45 @@
+﻿using AgentFire.Lifetime.ConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CApp.Lifetime("My app 1", A1, Z1);
+            CApp.Lifetime("My app 2", A2, Z2, false);
+        }
+
+        private static void A1()
+        {
+            Console.WriteLine("Starting...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Done.");
+        }
+        private static void Z1()
+        {
+            Console.WriteLine("Stopping...");
+            Thread.Sleep(300);
+            Console.WriteLine("Done.");
+        }
+
+        private static void A2()
+        {
+            Console.WriteLine("Starting...");
+            Thread.Sleep(1000);
+            throw new Exception();
+        }
+        private static void Z2()
+        {
+            Console.WriteLine("Stopping...");
+            Thread.Sleep(1500);
+            throw new Exception();
+        }
+    }
+}
